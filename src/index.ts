52a568a3c200178aec845f6a2bb5b1f99da28075
Ben Harris <benharris5@gmail.com>
2022-09-30 14:32:14 -0400

    Added source map generation

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import {
 } from "./promptsConfigs.js";
 import {SnoopInstance} from "./SnoopInstance.js";
 
-const quitOnCancel = {onCancel: () => process.exit(1)}
+const quitOnCancel = {onCancel: () => process.exit(1)};
 
 const answers = await prompts(loginPrompt, quitOnCancel);
 
